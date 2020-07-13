d5da31be28982aab2d13c2cd6816ebc8e8a9a439
larell mcbride <mcbridelarell@gmail.com>
2020-07-13 17:42:57 -0400

    completed base text game. more updates to come.

--- Game.java
+++ Game.java
@@ -24,7 +24,7 @@ public class Game {
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 28);
     JButton startButton, choice1, choice2, choice3, choice4;
     JTextArea mainTextArea;
-    int playerHP;
+    int playerHP, monsterHP, blueGem;
     String weapon, position;
 
     TitleScreenHandler tsHandler = new TitleScreenHandler();
@@ -63,7 +63,7 @@ public class Game {
         startButton.setFont(normalFont);
 
         startButton.addActionListener(tsHandler);// added action listener to start game
-        startButton.setFocusPainted(false); //removes extra white lines around text
+        startButton.setFocusPainted(false); // removes extra white lines around text
 
         startButtonPanel.add(startButton);
 
@@ -72,7 +72,7 @@ public class Game {
     }
 
     public void createGameScreen() {
-        // must disable these panels to activate 
+        // must disable these panels to activate
         titleNamePanel.setVisible(false);
         startButtonPanel.setVisible(false);
 
@@ -91,18 +91,18 @@ public class Game {
         mainTextPanel.add(mainTextArea);
 
         choiceButtonPanel = new JPanel();
-        choiceButtonPanel.setBounds(250,350,300,150);
+        choiceButtonPanel.setBounds(250, 350, 300, 150);
         choiceButtonPanel.setBackground(Color.black);
-        choiceButtonPanel.setLayout(new GridLayout(4,1));
+        choiceButtonPanel.setLayout(new GridLayout(4, 1));
         con.add(choiceButtonPanel);
-        //adding choice buttons to gamescreen
+        // adding choice buttons to gamescreen
         choice1 = new JButton("Choice 1");
         choice1.setBackground(Color.black);
         choice1.setForeground(Color.white);
         choice1.setFont(normalFont);
         choice1.setFocusPainted(false);
         choiceButtonPanel.add(choice1);
-        choice1.addActionListener(choiceHandler);//handles choices made
+        choice1.addActionListener(choiceHandler);// handles choices made
         choice1.setActionCommand("c1");
 
         choice2 = new JButton("Choice 2");
@@ -132,51 +132,52 @@ public class Game {
         choice4.addActionListener(choiceHandler);
         choice4.setActionCommand("c4");
 
-        //adding player stats(HP weapon choice)
-        playerPanel= new JPanel();
-        playerPanel.setBounds(100,15,600,50);
+        // adding player stats(HP weapon choice)
+        playerPanel = new JPanel();
+        playerPanel.setBounds(100, 15, 600, 50);
         playerPanel.setBackground(Color.black);
-        playerPanel.setLayout(new GridLayout(1,4));
+        playerPanel.setLayout(new GridLayout(1, 4));
         con.add(playerPanel);
-        //HP
+        // HP
         hpLabel = new JLabel("HP: ");
         hpLabel.setFont(normalFont);
         hpLabel.setForeground(Color.white);
         playerPanel.add(hpLabel);
-        //HP Number
+        // HP Number
         hpLabelNumber = new JLabel();
         hpLabelNumber.setFont(normalFont);
         hpLabelNumber.setForeground(Color.white);
         playerPanel.add(hpLabelNumber);
-        //weapon
+        // weapon
         weaponLabel = new JLabel("Weapon: ");
         weaponLabel.setFont(normalFont);
         weaponLabel.setForeground(Color.white);
         playerPanel.add(weaponLabel);
-        //weapon name
+        // weapon name
         weaponLabelName = new JLabel();
         weaponLabelName.setFont(normalFont);
         weaponLabelName.setForeground(Color.white);
         playerPanel.add(weaponLabelName);
-        //Calling the playerSetup Method
+        // Calling the playerSetup Method
         playerSetup();
-
-
 
     }
 
     public void playerSetup() {
         playerHP = 15;
+        monsterHP = 20;
         weapon = "Dem Hands";
-        weaponLabelName.setText(weapon);//displaying weapon name
-        hpLabelNumber.setText("" + playerHP); //displaying HP
+        weaponLabelName.setText(weapon);// displaying weapon name
+        hpLabelNumber.setText("" + playerHP); // displaying HP
 
         townGate(); // calls townGate Method
     }
+
     public void townGate() {
         position = "townGate";// Will show player's place
 
-        mainTextArea.setText("You are at the gate of the town.\n A Guard is standing in front of you.\n\n What do you do?");
+        mainTextArea
+                .setText("You are at the gate of the town.\n A Guard is standing in front of you.\n\n What do you do?");
 
         choice1.setText("Talk to the Guard");
         choice2.setText("Run the fade");
@@ -184,47 +185,305 @@ public class Game {
         choice4.setText("");
     }
 
-    public void talkGuard(){
+    public void talkGuard() {
         position = "talkGuard";
 
-        mainTextArea.setText("Hello there! I have never seen you in these parts?\n  I cannot let you pass.\n Sorry bout that...");
+        mainTextArea.setText(
+                "Hello there! I have never seen you in these parts?\n  I cannot let you pass.\n not with that monster out there...");
 
-        choice1.setText("Mean mug walk away");
+        choice1.setText("Walk away");
         choice2.setText("");
         choice3.setText("");
         choice4.setText("");
     }
 
-    public class TitleScreenHandler implements ActionListener{
+    public void attackGuard() {
+        position = "attackGuard";
 
-        public void actionPerformed(ActionEvent event){
+        mainTextArea.setText("Guard: What the What!? \n He proceeds to dropkick you.\n\n receive 3 damage ");
+        playerHP = playerHP - 3;
+        hpLabelNumber.setText("" + playerHP);
+
+        choice1.setText("Ouch! back to the gate");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void crossRoad() {
+        position = "crossRoad";
+
+        mainTextArea.setText("You are a crossroad.\n If you go South, you will go back to the town.");
+
+        choice1.setText("Go North");
+        choice2.setText("Go East");
+        choice3.setText("Go South");
+        choice4.setText("Go West");
+
+    }
+
+    public void north() {
+        position = "north";
+        mainTextArea
+                .setText("There is a river.\n You drink the water and rest at riverside\n\n(Your HP recovers by 2)");
+        playerHP = playerHP + 2;
+        hpLabelNumber.setText("" + playerHP);
+
+        choice1.setText("Go South");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void east() {
+        position = "east";
+        mainTextArea.setText("You find a Long sword resting on a tree\n \nObtained Long sword!!");
+        weapon = "Long Sword";
+        weaponLabelName.setText("Long Sword");
+
+        choice1.setText("Go West");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void west() {
+        position = "west";
+        mainTextArea.setText("You encounter a Monster!!");
+
+        choice1.setText("FIGHT!");
+        choice2.setText("RUN!!");
+        choice3.setText("");
+        choice4.setText("");
+
+    }
+
+    public void fight() {
+        position = "fight";
+        mainTextArea.setText("Monster HP: " + monsterHP + "\n \n What do you do?!");
+
+        choice1.setText("ATTACK!!!");
+        choice2.setText("RUN!!");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void playerAttack() {
+        position = "playerAttack";
+        // "battle System"
+        int playerDamage = 0; // your current damage
+
+        if (weapon.equals("Dem Hands")) {
+            playerDamage = new java.util.Random().nextInt(3);
+        } else if (weapon.equals("Long Sword")) {
+            playerDamage = new java.util.Random().nextInt(8);
+        }
+
+        mainTextArea.setText("You attacked the Monster and gave " + playerDamage + " Damage!");
+
+        monsterHP = monsterHP - playerDamage;
+
+        choice1.setText("ATTACK!!!");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+
+        // playerDamage = new java.util.Random().nextInt(3); //random number of damage
+        // player will output.
+    }
+
+    public void monsterAttack() {
+        position = "monsterAttack";
+
+        int monsterDamage = 0;
+
+        monsterDamage = new java.util.Random().nextInt(6);
+
+        mainTextArea.setText("Monster attacks!! you recive " + monsterDamage + " damage!");
+
+        playerHP = playerHP - monsterDamage;
+        hpLabelNumber.setText("" + playerHP);
+
+        choice1.setText("Attack again!");
+        choice2.setText("RUN!!");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void win() {
+        position = "win";
+
+        mainTextArea.setText(
+                "You dropped the ugly sucka! It dropped a Gem of some sort. Is this the monster \n the guard was talking about?? \n \n You obtained a blue Gem!! ");
+
+        blueGem = 1;
+
+        choice1.setText("Go East");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+
+    }
+
+    public void lose() {
+        position = "lose";
+
+        mainTextArea.setText("End of the road for you.\n \n Game Over!!");
+
+        choice1.setText("Try Again");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+        choice1.setVisible(false);
+        choice2.setVisible(false);
+        choice3.setVisible(false);
+        choice4.setVisible(false);
+    }
+
+    public void ending() {
+        position = "ending";
+
+        mainTextArea.setText(
+                "Guard: You killed the monster!?\n thank you so much! Please enter our town!! \n \n THE END??");
+
+        choice1.setText("");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+        choice1.setVisible(false);
+        choice2.setVisible(false);
+        choice3.setVisible(false);
+        choice4.setVisible(false);
+    }
+
+    public class TitleScreenHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
 
             createGameScreen();
         }
     }
-    public class ChoiceHandler implements ActionListener{
 
-        public void actionPerformed(ActionEvent event){
+    public class ChoiceHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
 
             String yourChoice = event.getActionCommand();
 
-            switch(position){
+            switch (position) {
                 case "townGate":
-                switch(yourChoice){
-                    case "c1": talkGuard(); break;
-                    case "c2": break;
-                    case "c3": break;
-                }
-                break;
+                    switch (yourChoice) {
+                        case "c1":
+                            if (blueGem == 1) {
+                                ending();
+                            } else {
+                                talkGuard();
+                            }
+                            break;
+                        case "c2":
+                            attackGuard();
+                            break;
+                        case "c3":
+                            crossRoad();
+                            break;
+                    }
+                    break;
                 case "talkGuard":
-                switch(yourChoice){
-                    case "c1": townGate(); break;
-                }
+                    switch (yourChoice) {
+                        case "c1":
+                            townGate();
+                            break;
+                    }
+                    break;
+                case "attackGuard":
+                    switch (yourChoice) {
+                        case "c1":
+                            townGate();
+                            break;
+                    }
+                    break;
+                case "crossRoad":
+                    switch (yourChoice) {
+                        case "c1":
+                            north();
+                            break;
+                        case "c2":
+                            east();
+                            break;
+                        case "c3":
+                            townGate();
+                            break;
+                        case "c4":
+                            west();
+                            break;
+                    }
+                    break;
+                case "north":
+                    switch (yourChoice) {
+                        case "c1":
+                            crossRoad();
+                            break;
+                    }
+                    break;
+                case "east":
+                    switch (yourChoice) {
+                        case "c1":
+                            crossRoad();
+                            break;
+                    }
+                    break;
+                case "west":
+                    switch (yourChoice) {
+                        case "c1":
+                            fight();
+                            break;
+                        case "c2":
+                            crossRoad();
+                            break;
+                    }
+                    break;
+                case "fight":
+                    switch (yourChoice) {
+                        case "c1":
+                            playerAttack();
+                            break;
+                        case "c2":
+                            crossRoad();
+                            break;
+                    }
+                    break;
+                case "playerAttack":
+                    switch (yourChoice) {
+                        case "c1":
+                            if (monsterHP < 1) {
+                                win();
+                            } else {
+                                monsterAttack();
+                            }
+                            break;
+                    }
+                    break;
+                case "monsterAttack":
+                    switch (yourChoice) {
+                        case "c1":
+                            if (playerHP < 1) {
+                                lose();
+                            } else {
+                                fight();
+                            }
+                            break;
+                    }
+                    break;
+                case "win":
+                    switch (yourChoice) {
+                        case "c1":
+                            crossRoad();
+                            break;
+                    }
+
             }
 
         }
     }
-
-    
 
 }
